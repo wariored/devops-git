bde78f60a0c76a3a6d3b5f0f1cd6a0e583e9af59
Cheikh Tidjane Konteye <wariored@Cheikhs-MBP.home>
2023-08-05 21:54:32 +0200

    add docker compose and add new endoints

--- main.py
+++ main.py
@@ -3,14 +3,25 @@ import redis
 
 app = FastAPI()
 
-#redis_client = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
+redis_client = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
 
 @app.get("/")
 def home():
     return {"message": "Hello, DIC2!"}
 
-#@app.get("/visited")
-#def read_user_visits():
-#    visits = redis_client.incr('visits')
-#    return {"Hellooooo": "DIC2!", "visits": visits}
+@app.get("/visited")
+def read_user_visits():
+    visits = redis_client.incr('visits')
+    return {"Hellooooo": "DIC2!", "visits": visits}
 
+
+@app.get("/users")
+def users():
+    return {"user": []}
+
+
+
+
+
+
+local files ->  volumes <-  container
