38cde685d678180703dc91c0145440c52a407260
Cheikh Tidjane Konteye <wariored@Cheikhs-MBP.technicolor.net>
2023-08-12 10:37:38 +0200

    add bunch of things: - fix tests - add github actions - add new requirements for linting and formatting

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
-from fastapi import FastAPI
 import redis
+from fastapi import FastAPI
+import collections
 
 app = FastAPI()
 
-redis_client = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
+redis_client = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+
 
 @app.get("/")
 def home():
     return {"message": "Hello, DIC2!"}
 
+
 @app.get("/visited")
 def read_user_visits():
-    visits = redis_client.incr('visits')
+    visits = redis_client.incr("visits")
     return {"Hellooooo": "DIC2!", "visits": visits}
 
 
 @app.get("/users")
 def users():
     return {"user": []}
-
-
-
-
-
-
-local files ->  volumes <-  container
--- test.py
+++ test.py
@@ -5,16 +5,17 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_root_endpoint():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, DIC2!"}
 
-@patch('main.redis_client.incr')
+
+@patch("main.redis_client.incr")
 def test_visited_endpoint(mock_incr):
-    mock_incr.return_value = 1
+    mock_incr.return_value = 2
 
     response = client.get("/visited")
     assert response.status_code == 200
-    assert "count" in response.json()
-
+    assert response.json() == {"Hellooooo": "DIC2!", "visits": 2}
